8efafebb54b32dc6a0857e295509c307d0cb043c
erandro <erandro@gmail.com>
2018-12-27 19:28:19 -0600

    edit CatCard to be in masonry-4 columns, added some style to cards

--- pintecatfront/src/components/CardWrapper/CardWrapper.js
+++ pintecatfront/src/components/CardWrapper/CardWrapper.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./CardWrapper.css";
 
-const CardWrapper = props => <div className="cardsWrapper">{props.children}</div>;
+const CardWrapper = props => <div className="masonry">{props.children}</div>;
 
 export default CardWrapper;
